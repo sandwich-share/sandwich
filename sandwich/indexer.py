0c74c455740396a8fab47f4432a2a73e20a5e6e7
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-03-09 01:48:08 -0500

    changed to global shared directory

--- sandwich/indexer.py
+++ sandwich/indexer.py
@@ -1,15 +1,16 @@
 import os
 index = []
+shared_directory = CONFIG["shared_directory"]
 
 # crawl a directory and find all files and folders
-def find_files(directory):
-  for path, dirs, files in os.walk(directory):
+def find_files():
+  for path, dirs, files in os.walk(shared_directory):
     for f in files:
       add_file(path, directory, f)
   return index
 
 # add a file and folder to the index
-def add_file(path, shared_directory, filename):
+def add_file(path, filename):
   index.append((os.path.relpath(path, shared_directory), filename))
 
 # remove a file from the index
