5990d8c30a0b073f7968aa0877851536b79cb60c
Cameron Gutman <cameron.gutman@case.edu>
2013-03-09 18:33:54 -0500

    Don't overwrite existing files. Handle socket errors more gracefully (part 1)

--- sandwich/client.py
+++ sandwich/client.py
@@ -1,5 +1,4 @@
 import httplib, json
-
 import config, files
 
 
@@ -7,15 +6,22 @@ class SandwichGetter(object):
 
     @classmethod
     def get_res(cls, ip, res):
-        conn = httplib.HTTPConnection(ip, timeout=config.timeout)
-        conn.request("GET", res)
-        r1 = conn.getresponse()
-        #TODO There should totally be error handling here
-        # we also will probably overwrite a file if it exists.
-        # oops.
-        with open(path.expanduser(config.shared_directory) + res, 'wb') as f:
-            files.stream_file(r1, f)
-        conn.close()
+    
+        try:
+            conn = httplib.HTTPConnection(ip, timeout=config.timeout)
+            conn.request("GET", res)
+            r1 = conn.getresponse()
+        
+            fullpath = path.expanduser(config.shared_directory) + res
+            if path.exists(fullpath):
+                print "Not overwriting existing file"
+            else:
+                with open(fullpath, 'wb') as f:
+                    files.stream_file(r1, f)
+
+            conn.close()
+        except socket.error:
+            print "Socket error while downloading"
 
     @classmethod
     def get_many_res(cls, ip, reses):
@@ -25,10 +31,13 @@ class SandwichGetter(object):
 
     @classmethod
     def bootstrap_into_network(cls, ip):
-        conn = httplib.HTTPConnection("%s:%d" % (ip, config.serverport), timeout=config.timeout)
-        conn.request("GET", "/neighbors")
-        r1 = conn.getresponse()
-        config.neighbors.extend(json.loads(r1.read()))
-        config.neighbors.append(ip)
-        config.neighbors = list(set(config.neighbors))
-        conn.close()
+        try:
+            conn = httplib.HTTPConnection("%s:%d" % (ip, config.serverport), timeout=config.timeout)
+            conn.request("GET", "/neighbors")
+            r1 = conn.getresponse()
+            config.neighbors.extend(json.loads(r1.read()))
+            config.neighbors.append(ip)
+            config.neighbors = list(set(config.neighbors))
+            conn.close()
+        except socket.error:
+            print "Error bootstrapping to %s:%d" % (ip, config.serverport)
