dd335f6f9e1ae875c8c45ca744168933dd4d4cd3
Aaron Neyer <aaronneyer@gmail.com>
2013-03-09 08:07:06 -0500

    SOme basic changes for the web app

--- sandwich/webapp.py
+++ sandwich/webapp.py
@@ -5,16 +5,18 @@ import os
 
 import indexer
 
-app = Flask(__name__)
+indexer.find_files()
 app = Flask('webapp', template_folder=os.getcwd() + "/templates")
 
 @app.route("/")
 def index():
-  indexer.find_files()
-  print len(indexer.index)
-  return render_template("index.html", index=indexer.index)
+    return render_template("index.html")
+
+@app.route("/query", methods=["POST"])
+def query():
+    return render_template("query_result.html", index=indexer.index)
 
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+    app.debug = True
+    app.run()
