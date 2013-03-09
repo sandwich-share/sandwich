5edcc72ecfad0e10fb094f7347f4054b51e2870a
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-03-09 05:36:48 -0500

    fixed repeating index. fixed ~/ shortcut. added dump to config.index

--- sandwich/indexer.py
+++ sandwich/indexer.py
@@ -2,20 +2,26 @@ import os
 import config
 
 index = []
-shared_directory = config.shared_directory
+shared_directory = os.path.expanduser(config.shared_directory)
 
 # crawl a directory and find all files and folders
 def find_files():
-  for path, dirs, files in os.walk(os.path.expanduser(shared_directory)):
-    for f in files:
-      if not f.startswith('.') and not os.path.split(path)[1].startswith('.'):
-        add_file(path, f)
-  return index
+    global index
+    index = []
+    for path, dirs, files in os.walk(shared_directory):
+        for f in files:
+            if not f.startswith('.') and not os.path.split(path)[1].startswith('.'):
+                add_file(path, f)
+    config.index = index
+    return index
 
 # add a file and folder to the index
 def add_file(path, filename):
-  index.append((os.path.relpath(path, shared_directory), filename))
+    print path
+    index.append((os.path.relpath(path, shared_directory), filename))
+    config.index = index
 
 # remove a file from the index
 def remove_file(path, filename):
-  index.remove((path, filename))
+    index.remove((path, filename))
+    config.index = index
