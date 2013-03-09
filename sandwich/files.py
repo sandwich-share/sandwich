a992389cab67e99b87ec3fa8c23b5090d10fcc26
JDulin <dulinjd@gmail.com>
2013-03-09 18:18:49 -0500

    Streaming incomplete downloads

--- sandwich/files.py
+++ sandwich/files.py
@@ -1,6 +1,24 @@
-import config
+import config, os
 
 def stream_file(fin, fout):
+    shared = config.shared_directory
+    try:
+        with open(shared + "/" + fin, "r") as f:
+            size = os.stat(shared + fin).st_size
+
+
+            if size == os.stat(fin).st_size:
+                return
+            
+            fin.seek(size + 1)
+            while True:
+                chunk = fin.read(config.chunk_size)
+                if not chunk: break
+                fout.write(chunk)
+
+    except IOError as e:
+        return None
+        
     while True:
         chunk = fin.read(config.chunk_size)
         if not chunk: break
