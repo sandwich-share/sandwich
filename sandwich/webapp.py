022b250162944dce19f1d6711e80a0dd7045fc7a
Aaron Neyer <aaronneyer@gmail.com>
2013-03-09 18:11:05 -0500

    Prettier

--- sandwich/webapp.py
+++ sandwich/webapp.py
@@ -34,23 +34,23 @@ def neighbors():
 
 @app.route("/search", methods=["GET"])
 def search():
-    x = ""
+    x = []
     if not request.args.get("host"):
         for n in config.neighbors:
             conn = httplib.HTTPConnection("%s:%d" % (n, config.webapp),
                                           timeout=config.timeout)
             conn.request("GET", "/query",
                     urllib.urlencode({'search': request.args.get("search"), 'ip': n}))
-            x = conn.getresponse().read()
+            x += json.loads(conn.getresponse().read())
             conn.close()
     else:
         conn = httplib.HTTPConnection("%s:%d" % (request.args.get("host"),
                                                  config.webapp), timeout=config.timeout)
 
         conn.request("GET", "/query", urllib.urlencode({'search': "", 'ip': request.args.get("host")}))
-        x = conn.getresponse().read()
+        x += conn.getresponse().read()
         conn.close()
-    return x
+    return render_template("query_result.html", index=x)
 
 def run():
 
