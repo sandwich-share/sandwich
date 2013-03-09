e337ecfa0e16afa3bac8eb8bfabf50d7be50de5d
John Gunderman <johngunderman@gmail.com>
2013-03-09 15:13:34 -0500

    ACTUALLY fixed the merge this time

--- sandwich/webapp.py
+++ sandwich/webapp.py
@@ -36,13 +36,8 @@ def search():
             conn.close()
 
     else:
-<<<<<<< HEAD
         conn = httplib.HTTPConnection("%s:%d" % (request.args["host"], config.webapp), timeout=config.timeout)
-        conn.request("GET", "/query", urllib.urlencode({'search': request.form.get("search")}))
-=======
-        conn = httplib.HTTPConnection("%s:%d" % (request.args["host"], config.webapp))
         conn.request("GET", "/query", urllib.urlencode({'search': request.args.get("search")}))
->>>>>>> 1c1da62045aca06caa4b82855a89be210b07e11c
         y = conn.getresponse().read()
         print y
         x += y
