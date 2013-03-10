b22edbc6382daf877599bab14009853b1cd855e9
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-03-10 01:53:27 -0500

    Fixed watch dog

--- sandwich/file_monitor.py
+++ sandwich/file_monitor.py
@@ -6,20 +6,22 @@ from watchdog.events import FileSystemEventHandler
 import indexer
 import config
 
+shared_directory = config.shared_directory
+
 class SimpleEventHandler(FileSystemEventHandler):
     def on_created(self, event):
         super(SimpleEventHandler, self).on_created(event)
 
         if (not event.is_directory):
             p = event.src_path
-            indexer.add_file(p, os.path.split(p)[1])
+            indexer.add_file(os.path.relpath(os.path.split(p)[0], shared_directory), os.path.split(p)[1])
 
     def on_deleted(self, event):
         super(SimpleEventHandler, self).on_created(event)
 
         if (not event.is_directory):
             p = event.src_path
-            indexer.remove_file(p, os.path.split(p)[1])
+            indexer.remove_file(os.path.relpath(os.path.split(p)[0], shared_directory), os.path.split(p)[1])
 
     def on_moved(self, event):
         super(SimpleEventHandler, self).on_created(event)
@@ -27,13 +29,13 @@ class SimpleEventHandler(FileSystemEventHandler):
         if (not event.is_directory):
             ps = event.src_path
             pd = event.dest_path
-            indexer.remove_file(ps, os.path.split(ps)[1])
-            indexer.add_file(pd, os.path.split(pd)[1])
+            indexer.remove_file(os.path.relpath(os.path.split(ps)[0], shared_directory), os.path.split(ps)[1])
+            indexer.add_file(os.path.relpath(os.path.split(pd)[0], shared_directory), os.path.split(pd)[1])
 
 def start():
     observer = Observer()
     if (len(sys.argv) == 1):
-        observer.schedule(SimpleEventHandler(), config.shared_directory, recursive=True)
+        observer.schedule(SimpleEventHandler(), shared_directory, recursive=True)
     else:
         for i in range(1, len(sys.argv)):
             observer.schedule(SimpleEventHander(), sys.argv[i], recursive=True)
