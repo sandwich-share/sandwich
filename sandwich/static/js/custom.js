6967508603783113dd9a92770ace3c14f90536b2
Aaron Neyer <aaronneyer@gmail.com>
2013-03-10 05:06:59 -0400

    Keeps track of content to keep triggers updated

--- sandwich/static/js/custom.js
+++ sandwich/static/js/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         $.get("/search", {search:
             $(this).find("input[type=text]").val()}, function(data){
                 $("#content").html(data);
+                $("#content").trigger("change");
             });
     })
     $("#peer-list td a").on("click", function(e) {
@@ -11,10 +12,12 @@ $(document).ready(function(){
         $("#query_form input[type=text]").val("");
         $.get("/search", {host: $(this).attr("data-host")}, function(data){
             $("#content").html(data);
+            $("#content").trigger("change");
         });
     })
-    $(".dl-link").on("click", function(e) {
+    x = function(){ $(".dl-link").on("click", function(e) {
         e.preventDefault();
         $.get("/download", {url: $(this).attr("data-url")});
-    })
+    })}
+    $("#content").on("change", x);
 })
